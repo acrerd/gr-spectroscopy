9cdbfa039ccafa992ba5a27a280d2de370e6f9c4
ronnie <frith.ronnie@gmail.com>
2015-08-05 14:51:42 +0100

    Fixed initial commit.

--- build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/astro/repositories/gnuradio_modules/gr-spectroscopy/apps
-# Build directory: /home/astro/repositories/gnuradio_modules/gr-spectroscopy/build/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/astro/repositories/gnuradio_modules/gr-spectroscopy/lib
-# Build directory: /home/astro/repositories/gnuradio_modules/gr-spectroscopy/build/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
